42257fd5336582da6bb1625a3e28f19222df4cd9
Daniel Trinidad <daniuy524@gmail.com>
2024-11-09 10:48:38 -0600

    feat(HOC): :white_check_mark: New example added

--- src/components/HighOrderComponent/HighOrderComponent.tsx
+++ src/components/HighOrderComponent/HighOrderComponent.tsx
@@ -1,6 +1,40 @@
+import { useState } from "react";
+
+interface Props {
+  Component : React.ComponentType;
+  isLoading : boolean;
+}
+function withLoader ({Component, isLoading} : Props) {
+  return () => {
+    return isLoading ? <div>Loading...</div> : <Component />;
+  }
+}
+
+
 export const ParentComponent = () => {
+  const [isLoading, setIsLoading] =  useState(false);
+  const [isDisabled, setIsDisabled] =  useState(false);
+
+  const handleClick = () => {
+    setIsLoading(true);
+    setIsDisabled(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+      setIsDisabled(false);
+    }, 2000);
+  }
   return (
     <div>
+      <button
+        onClick={handleClick}
+        disabled={isDisabled}
+      >
+        Click
+      </button>
+      {
+        withLoader({Component: () => <p>Componente</p>, isLoading})()
+      }
     </div>
   );
 };
