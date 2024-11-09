7b53a069e54864a76b6663d1111dd660e7dd9e2e
Daniel Trinidad <daniuy524@gmail.com>
2024-11-09 11:46:53 -0600

    feat(custom Hook): New example added

--- src/components/CustomHooks/CustomHooks.tsx
+++ src/components/CustomHooks/CustomHooks.tsx
@@ -1,6 +1,43 @@
+import { useState } from "react";
+
+function useLoading (initialValue: boolean) {
+  const [isLoading, setIsLoading] =  useState(initialValue);
+  const [isDisabled, setIsDisabled] =  useState(initialValue);
+
+  const handleClick = () => {
+    setIsLoading(true);
+    setIsDisabled(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+      setIsDisabled(false);
+    }, 2000);
+  }
+
+  return {
+    isLoading,
+    isDisabled,
+    handleClick
+  }
+} 
+
 const UserComponent = () => {
+
+  const {isLoading, isDisabled, handleClick} = useLoading(false);
+  
+  
+
   return (
     <div>
+      <button
+        onClick={handleClick}
+        disabled={isDisabled}
+      >
+        Click
+      </button>
+      {
+        isLoading ? <p>Loading...</p> : <p>Componente</p>
+      }
     </div>
   );
 };
