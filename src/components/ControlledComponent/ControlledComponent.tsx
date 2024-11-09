4fc2ef9bd990f49299dfba8003400f2306eeb6b3
Daniel Trinidad <daniuy524@gmail.com>
2024-11-09 12:14:28 -0600

    feat(controled component):

--- src/components/ControlledComponent/ControlledComponent.tsx
+++ src/components/ControlledComponent/ControlledComponent.tsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
+
 export const ControlledInput = () => {
+  
+  const [value, setValue] = useState('');
+
   return (
     <div>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Type here..."
+      />
+      <p>{value}</p>
     </div>
   );
 };
