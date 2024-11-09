2bc13c3953b3482167517145ca907b3d34fc0584
Daniel Trinidad <daniuy524@gmail.com>
2024-11-09 12:24:03 -0600

    feat(uncontrolled component):

--- src/components/UncontrolledComponent/UncontrolledComponent.tsx
+++ src/components/UncontrolledComponent/UncontrolledComponent.tsx
@@ -1,6 +1,22 @@
+import { useRef } from "react";
+
 export const UncontrolledInput = () => {
+
+  const ref = useRef<HTMLInputElement>(null);
+
+  const handleSubit = (e: React.FormEvent<HTMLFormElement>) => {
+
+    e.preventDefault();
+      console.log(ref.current?.value);
+    
+  }
+
   return (
     <div>
+      <form onSubmit={handleSubit}>
+        <input type="text" placeholder="Type here..." ref={ref}/>
+        <button type="submit">Click</button>
+      </form>
     </div>
   );
 };
