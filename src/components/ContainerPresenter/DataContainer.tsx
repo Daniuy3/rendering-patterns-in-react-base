d0756a398278e5cbef3d0df259299e306046adc2
Daniel Trinidad <daniuy524@gmail.com>
2024-11-09 12:10:21 -0600

    feat(presenter-container):

--- src/components/ContainerPresenter/DataContainer.tsx
+++ src/components/ContainerPresenter/DataContainer.tsx
@@ -1,6 +1,25 @@
+import { useEffect, useState } from 'react';
+import mainData from '../../../public/data/data.json';
+import { DataPresenter } from './DataPresenter';
+
+interface DataItem {
+  id: number;
+  name: string;
+  description: string;
+  image: string;
+}
+
 export const DataContainer = () => {
+
+  const [ data, setData ] = useState<DataItem[]>([]);
+
+  useEffect(() => {
+    setData(mainData);
+  },[])
+
   return (
     <div>
+      <DataPresenter data={data} />
     </div>
   );
 };
--- src/components/ContainerPresenter/DataPresenter.tsx
+++ src/components/ContainerPresenter/DataPresenter.tsx
@@ -1,6 +1,28 @@
-export const DataPresenter = () => {
+interface DataItem {
+  id: number;
+  name: string;
+  description: string;
+  image: string;
+}
+
+interface Props {
+  data: DataItem[];
+}
+
+export const DataPresenter = ( { data } : Props) => {
+
   return (
     <>
+    {
+      data.map((item) => {
+        return (
+          <div key={item.id}>
+            <h2>{item.name}</h2>
+            <p>{item.description}</p>
+          </div>
+        );
+      })
+    }
     </>
   );
 };
